56246b4a87196e9cb6a9cac5fac5664a99bd4a54
pierriperim-prodentis <pierri.perim@prodentis.com.br>
2026-09-15 11:04:12 -0300

    Update 2_Funil.py

--- pages/2_Funil.py
+++ pages/2_Funil.py
@@ -2,15 +2,6 @@ import os
 
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-        .block-container {padding: 0 !important;}
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 html_path = os.path.join(os.path.dirname(__file__), "..", "Dashboard_Funil.html")
 
 with open(html_path, "r", encoding="utf-8") as f:

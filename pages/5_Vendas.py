4a16c829b66ef29285f0aae9d4fa392fb01c0879
pierriperim-prodentis <pierri.perim@prodentis.com.br>
2026-09-15 11:04:45 -0300

    Update 5_Vendas.py

--- pages/5_Vendas.py
+++ pages/5_Vendas.py
@@ -2,15 +2,6 @@ import os
 
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-        .block-container { padding: 0 !important; }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 html_path = os.path.join(os.path.dirname(__file__), "..", "relatorio.html")
 
 with open(html_path, "r", encoding="utf-8") as f:

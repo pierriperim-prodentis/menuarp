5c51bf1e70a6a0f931eb47c58112ed232dfd4e6d
pierriperim-prodentis <pierri.perim@prodentis.com.br>
2026-09-15 11:34:09 -0300

    Update 5_Vendas.py

--- pages/5_Vendas.py
+++ pages/5_Vendas.py
@@ -3,27 +3,8 @@ import os
 import streamlit as st
 
 html_path = os.path.join(os.path.dirname(__file__), "..", "relatorio.html")
+
 with open(html_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
-auto_resize = """
-<script>
-(function() {
-    function sendHeight() {
-        var h = document.body.scrollHeight || document.documentElement.scrollHeight;
-        window.parent.postMessage({type: 'streamlit:setFrameHeight', height: h}, '*');
-    }
-    window.addEventListener('load', function() {
-        sendHeight();
-        setTimeout(sendHeight, 500);
-        setTimeout(sendHeight, 1500);
-    });
-    window.addEventListener('resize', sendHeight);
-    var obs = new MutationObserver(sendHeight);
-    obs.observe(document.body, {childList: true, subtree: true, attributes: true});
-})();
-</script>
-"""
-html_content = html_content.replace("</body>", auto_resize + "</body>")
-
-st.components.v1.html(html_content, height=1400, scrolling=False)
+st.components.v1.html(html_content, height=6000, scrolling=False)

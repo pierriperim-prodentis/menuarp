636576df1786af09e62c8f83359ca535159d6d2d
pierriperim-prodentis <pierri.perim@prodentis.com.br>
2026-09-24 13:56:23 -0300

    Update 5_Vendas.py

--- pages/5_Vendas.py
+++ pages/5_Vendas.py
@@ -7,4 +7,4 @@ html_path = os.path.join(os.path.dirname(__file__), "..", "relatorio.html")
 with open(html_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
-st.components.v1.html(html_content, height=5100, scrolling=False)
+st.components.v1.html(html_content, height=5050, scrolling=False)
